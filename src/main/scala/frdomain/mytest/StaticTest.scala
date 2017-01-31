8794e1d899b65b582acb089489b9d8a9ed326470
rosorio <rosorio@sura.com.co>
2017-01-31 12:48:17 -0500

    se ajusta el llamado con polimorfismo

--- src/main/scala/frdomain/mytest/StaticTest.scala
+++ src/main/scala/frdomain/mytest/StaticTest.scala
@@ -30,7 +30,7 @@ case class MoneyMarketAccount(number:String,name:String,money:Int,rateOfInterest
 
 trait AccountService {
   type DateRange = Date
-  def calculateInterest(account: Account,
+  def calculateInterest [A <: InterestBearingAccount](account: A,
                                                      period: DateRange): Try[BigDecimal] = {
     def calculateInterest() : Reader[Account,BigDecimal] = Reader{
       case account:Account =>  account.money * 2
@@ -40,7 +40,7 @@ trait AccountService {
       case account:Account =>  account.money * 3
     }
 
-    val interest: Kleisli[Id, Account, BigDecimal] = for{
+    val interest: Reader[Account, BigDecimal] = for{
       a <- calculateInterest()
       b <- calculateInterest2()
     } yield(a+b)
--- src/test/scala/myowntest/MyOwnTest.scala
+++ src/test/scala/myowntest/MyOwnTest.scala
@@ -3,6 +3,7 @@ package myowntest
 import java.util.Date
 
 import frdomain.mytest.{Account, AccountService, MoneyMarketAccount}
+import org.scalactic.Fail
 import org.scalatest.FunSuite
 
 import scala.runtime.Nothing$
@@ -11,11 +12,11 @@ import scala.util.{Success, Try}
 class MyOwnTest extends FunSuite {
 
   test("Interest Test") {
-    val account: Account = MoneyMarketAccount("1234","rick",1,BigDecimal(100))
+    val account: MoneyMarketAccount = MoneyMarketAccount("1234","rick",1,BigDecimal(100))
     object AccountService extends AccountService
     import AccountService._
     val date: Date = new Date
-    val result: Try[BigDecimal] =calculateInterest(account,date)
+    val result: Try[BigDecimal] =calculateInterest[MoneyMarketAccount](account,date)
     assert(result == Success(BigDecimal(5)))
   }
 
